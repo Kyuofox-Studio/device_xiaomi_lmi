3c089e0d86cfd2e1a4068966cc81b962b45ed484
Alexander Koskovich <akoskovich@pm.me>
2022-06-28 06:17:07 -0700

    lmi: Move away from Bluetooth split makefiles
    Change-Id: I2bc63774a0420ea1c3d51a32ee149a9dc7a17063

--- device.mk
+++ device.mk
@@ -153,12 +153,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
 # Bluetooth
--include vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-board.mk
-
-BOARD_HAVE_QCOM_FM := false
-
-$(call inherit-product-if-exists, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-system-opensource-product.mk)
-
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.1-impl \
@@ -167,11 +161,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
-
-ifneq (,$(wildcard vendor/qcom/opensource/commonsys/system/bt))
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.bluetooth.library_name=libbluetooth_qti.so
-endif
 
 # Camera
 PRODUCT_PACKAGES += \
