55165e7eadaf0eef8a43f9180c57578da4eabdd0
kubersharma001 <kubersharma001@gmail.com>
2022-10-10 23:12:21 +0530

    lmi: Compile libldacBT_bco.so
    * E a2dp_vendor_ldac_decoder: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library libldacBT_bco.so not found

--- device.mk
+++ device.mk
@@ -166,6 +166,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio-impl \
     libbthost_if \
+    libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
